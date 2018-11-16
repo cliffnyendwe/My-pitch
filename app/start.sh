2f383fd0c7ce20b362ef9208edc8541176f27dcd
cliffnyendwe"git config --global user.name "cliffnyendwe"git commit -m"add fix indentation error"git status <cliffnyendwe>
2018-11-16 07:23:53 +0300

    installed migration

--- app/start.sh
+++ app/start.sh
@@ -1,1 +1,2 @@
+SECRET_KEY ='0710755276'
 python3.6 manage.py server
--- config.py
+++ config.py
@@ -1,42 +1,43 @@
 import os
-
 class Config:
-  """  
-  General configuration parent class
-  """
-  SECRET_KEY = os.environ.get('SECRET_KEY')
-  SQLALCHEMY_TRACK_MODIFICATIONS = False
-  UPLOADED_PHOTOS_DEST = 'app/static/photos'
-  MAIL_SERVER = 'smtp.googlemail.com'
-  MAIL_PORT = 587
-  MAIL_USE_TLS = True
-  MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
-  MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringaschool:0710755176cliff@localhost/pitch'
+    UPLOADED_PHOTOS_DEST = 'app/static/photos'
+    MAIL_SERVER = 'smtp.gmail.com'
+# email configuration
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
 
-class TestConfig(Config):
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://wanyeki:cartman@localhost/peach'  
+    # simple mde configurations
+    SIMPLEMDE_JS_IIFE = True
+    SIMPLEMDE_USE_CDN = True
+    
+    @staticmethod
+    def init_app(app):
+        pass
 
 
 class ProdConfig(Config):
-  """ 
-  Production configuration child class
-  Args:
-    Config: The parent configuration class with General
-    configuration settings
-  """
-  SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    pass
 
 
 class DevConfig(Config):
-  """  
-  Development configuration child class
-  Args:
-    Config: The parent configuration class with General
-    configuration settings
-  """
-  SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://wanyeki:cartman@localhost/peach'
-  DEBUG = True
+    DEBUG = True
 
+config_options = {
+'development':DevConfig,
+'production':ProdConfig
+
+}
+
+class TestConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringaschool:0710755176cliff@localhost/pitch_test'
+
+class DevConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringaschool:0710755176cliff@localhost/pitch'
+    DEBUG = True
 
 config_options = {
 'development':DevConfig,
