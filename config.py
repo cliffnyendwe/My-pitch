321d70f3b519e49ec4be710a5678d7cf234f7178
cliffnyendwe"git config --global user.name "cliffnyendwe"git commit -m"add fix indentation error"git status <cliffnyendwe>
2018-11-19 11:48:00 +0300

    updates

--- config.py
+++ config.py
@@ -44,7 +44,7 @@ class Config:
     # SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://yego:pass123@localhost/minutepitch'
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
 
-    SECRET_KEY=os.environ.get('SECRET_KEY')
+    SECRET_KEY=os.environ.get('SECRET_KEY')or '123'
     MAIL_SERVER ='smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
