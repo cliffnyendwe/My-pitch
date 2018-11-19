c0c6c0576ec74a09ec82b1b1a34764f1aa077e22
cliffnyendwe"git config --global user.name "cliffnyendwe"git commit -m"add fix indentation error"git status <cliffnyendwe>
2018-11-19 11:01:33 +0300

    sd

--- config.py
+++ config.py
@@ -20,7 +20,7 @@ class Config:
     SIMPLEMDE_USE_CDN = True
 
 class ProdConfig(Config):
-    SQLALCHEMY_DATABASE_URI = os.environ.get("HEROKU_POSTGRESQL_NAVY_URL")
+    SQLALCHEMY_DATABASE_URI = os.environ.get("HEROKU_POSTGRESQL_ORANGE_URL")
     DEBUG = True
     
 
