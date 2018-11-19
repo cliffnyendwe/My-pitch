c1ef6cc988ad329590e9cad0fc9258bc3914a7f6
cliffnyendwe"git config --global user.name "cliffnyendwe"git commit -m"add fix indentation error"git status <cliffnyendwe>
2018-11-19 09:39:39 +0300

    added styles

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
-SECRET_KEY='0710755176'
-MAIL_USERNAME='cliffnyendwe2018@gmail.com'
-MAIL_PASSWORD='nyendwe12'
-DATABASE_URL='postgresql+psycopg2://moringaschool:0710755176cliff@localhost/pitch'
+export SECRET_KEY='0710755176'
+export MAIL_USERNAME='cliffnyendwe2018@gmail.com'
+export MAIL_PASSWORD='nyendwe12'
+export DATABASE_URL='postgresql+psycopg2://moringaschool:0710755176cliff@localhost/pitch'
 python3.6 manage.py server
